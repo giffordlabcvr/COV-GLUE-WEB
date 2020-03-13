c7dc365e3db4b52a871edbf30eef3a6c9b1704e4
Josh Singer <josh.singer@glasgow.ac.uk>
2020-03-13 16:30:59 +0000

    added project version to versioning page.

--- WebContent/controllers/covVersioning.js
+++ WebContent/controllers/covVersioning.js
@@ -11,5 +11,19 @@ covApp.controller('covVersioningCtrl',
 				$scope.glueEngineVersion = data.glueEngineShowVersionResult.glueEngineVersion;
 			})
 			.error(glueWS.raiseErrorDialog(dialogs, "retrieving GLUE engine version"));
+
+			glueWS.runGlueCommand("", {
+			    "show":{
+			        "setting":{
+			            "settingName":"project-version"
+			        }
+			    }
+			})
+			.success(function(data, status, headers, config) {
+				$scope.covGlueProjectVersion = data.projectShowSettingResult.settingValue;
+			})
+			.error(glueWS.raiseErrorDialog(dialogs, "retrieving project-version setting"));
+			
+			
 			
 		} ]);

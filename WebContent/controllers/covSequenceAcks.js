1915e8f168d9d5ca24ac40303a4874de65d5ba0c
Josh Singer <josh.singer@glasgow.ac.uk>
2020-04-27 12:08:40 +0100

    updated stuff to do with GISAID sequence acknowledgements.

--- WebContent/controllers/covSequenceAcks.js
+++ WebContent/controllers/covSequenceAcks.js
@@ -1,29 +1,45 @@
 covApp.controller('covSequenceAcksCtrl', 
-		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', 'pagingContext', 
-			  function($scope, $route, $routeParams, glueWS, dialogs, pagingContext) {
+		[ '$scope', '$route', '$routeParams', 'glueWS', 'dialogs', '$controller', 'pagingContext', 
+			  function($scope, $route, $routeParams, glueWS, dialogs, $controller, pagingContext) {
+			$controller('sequencesCtrl', { $scope: $scope, 
+				glueWebToolConfig: glueWebToolConfig, 
+				glueWS: glueWS, 
+				dialogs: dialogs});
 
-				addUtilsToScope($scope);
+			console.log("initializing cov sequences");
 
-				$scope.sequences = [];
+			$scope.init("source.name = 'cov-gisaid'", 
+					["m49_country.display_name",
+		                "sequenceID",
+		                "isolate",
+		                "gisaid_originating_lab",
+		                "gisaid_submitting_lab",
+		                "gisaid_authors"] );
+			
+			$scope.initGlobalRegionFixedValueSetM49();
+			$scope.initDevelopmentStatusFixedValueSetM49();
 
-				glueWS.runGlueCommand("", {
-				    "list":{
-				        "sequence":{
-				            "fieldName":[
-				                "m49_country.display_name",
-				                "sequenceID",
-				                "isolate",
-				                "gisaid_originating_lab",
-				                "gisaid_submitting_lab",
-				                "gisaid_authors"
-				            ]
-				        }
-				    }
-				})
-			    .success(function(data, status, headers, config) {
-			    	$scope.sequences = tableResultAsObjectList(data);
-			    	console.log("$scope.sequences", $scope.sequences);
-			    })
-			    .error(glueWS.raiseErrorDialog(dialogs, "retreiving sequence data"));
+			
+			$scope.pagingContext.setDefaultSortOrder([
+  			    { property: "sequenceID", displayName: "GISAID Accession ID", order: "+" }
+  			]);
 
-		}]);
+			$scope.pagingContext.setSortableProperties([
+	            { property:"sequenceID", displayName: "GISAID Accession ID" },
+  	            { property:"m49_country.id", displayName: "Country of Origin" },
+	            { property:"isolate", displayName: "Virus name" }
+	        ]);
+
+			$scope.pagingContext.setFilterProperties([
+           		{ property:"sequenceID", displayName: "GISAID Accession ID", filterHints: {type: "String"} },
+  	            { property:"m49_country.display_name", nullProperty:"m49_country", altProperties:["m49_country.id"], displayName: "Country of Origin", filterHints: {type: "String"} },
+  	            $scope.globalRegionFilterM49(),
+  	            $scope.developmentStatusFilterM49(),
+  	            { property:"isolate", displayName: "Virus name", filterHints: {type: "String"} },
+  			]);
+
+  			$scope.pagingContext.setDefaultFilterElems([]);
+			
+			
+}]);
+

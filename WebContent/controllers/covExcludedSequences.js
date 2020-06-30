83734311c365cbcbdff1582381322a87ba8cc8ee
Josh Singer <josh.singer@glasgow.ac.uk>
2020-06-30 18:06:27 +0100

    excluded sequences page displays number of sequences (a) retrieved and (b) passing exclusion criteria.

--- WebContent/controllers/covExcludedSequences.js
+++ WebContent/controllers/covExcludedSequences.js
@@ -2,5 +2,31 @@ covApp.controller('covExcludedSeqsCtrl',
 		[ '$scope', 'glueWS', 'dialogs', 
 		function($scope, glueWS, dialogs) {
 
+			glueWS.runGlueCommand("custom-table-row/cov_project_properties/sequencesRetrieved", {
+			    "show":{
+			        "property":{
+			            "property":"value"
+			        }
+			    }
+			})
+			.success(function(data, status, headers, config) {
+				$scope.sequencesRetrieved = data.propertyValueResult.value;
+				console.info('$scope.sequencesRetrieved', $scope.sequencesRetrieved);
+			})
+			.error(glueWS.raiseErrorDialog(dialogs, "retrieving sequencesRetrieved project property"));
+
+			glueWS.runGlueCommand("custom-table-row/cov_project_properties/sequencesPassingExclusion", {
+			    "show":{
+			        "property":{
+			            "property":"value"
+			        }
+			    }
+			})
+			.success(function(data, status, headers, config) {
+				$scope.sequencesPassingExclusion = data.propertyValueResult.value;
+				console.info('$scope.sequencesPassingExclusion', $scope.sequencesPassingExclusion);
+			})
+			.error(glueWS.raiseErrorDialog(dialogs, "retrieving sequencesPassingExclusion project property"));
+
 			
 		} ]);
